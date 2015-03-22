472cf415551eb105c94168af7848e1ecf567a657
Joe Ryan <jryan@jryan-macbook.local>
2015-03-22 11:24:28 -0500

    Uninstalled fm-state

--- bower_components/fm-state/app/scripts/constants/fmStateConstants.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('fm.state').constant('fmStateConstants', {
-  'tokenHeader': 'X-FM-AUTH-Token',
-  'tokenStorageKey': 'fmtoken',
-  'returnToUrlKey': 'returnToUrl'
-});
--- bower_components/fm-state/app/scripts/services/qStateParams.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('fm.state').factory('$qStateParams', function() {
-  return {};
-});
